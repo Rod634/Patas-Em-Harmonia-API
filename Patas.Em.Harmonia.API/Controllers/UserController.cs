ab90946dbdc96bb01188cace916c7bda26271a9f
rodrigo <rodrigocsilva63@gmail.com>
2023-08-10 20:23:31 -0300

    remove unnecessary files and create user controller

---
+++ Patas.Em.Harmonia.API/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Patas.Em.Harmonia.API.Controllers
+{
+    [ApiController]
+    [Route("v1/[controller]")]
+    public class UserController : ControllerBase
+    {
+       
+    }
+}
