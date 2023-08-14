bf309a6bea50dee0e82b99822d82a32ebd075c66
rodrigo <rodrigocsilva63@gmail.com>
2023-08-14 17:11:53 -0300

    create IUserRepository with all needs

---
+++ Patas.Em.Harmonia.Infrastructure.Data/Interfaces/IUserRepository.cs
@@ -0,0 +1,25 @@
+﻿using Patas.Em.Harmonia.Infrastructure.Data.Models;
+
+namespace Patas.Em.Harmonia.Infrastructure.Data.Interfaces
+{
+    public interface IUserRepository
+    {
+        /// <summary>
+        /// Get an user data by email
+        /// </summary>
+        /// <returns>Returns an User Data</returns>
+        Task<User> GetUserByMail(string email);
+
+        /// <summary>
+        /// Change a user info
+        /// </summary>
+        /// <returns>An user with the new data</returns>
+        Task<User> ChangeUserData(User userNewData);
+
+        /// <summary>
+        /// Delete a user by mail
+        /// </summary>
+        /// <returns>A bool that indicates success or not</returns>
+        Task<bool> DeleteUserByEmail(string email);
+    }
+}
