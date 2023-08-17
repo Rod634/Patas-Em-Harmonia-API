d624f5e57e8d426ff4e3c9d67d4b9cc53804bb78
rodrigo <rodrigocsilva63@gmail.com>
2023-08-16 21:59:39 -0300

    Add user validations

---
+++ Patas.Em.Harmonia.Domain/Validators/UserValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using Patas.Em.Harmonia.Domain.Models;
+
+namespace Patas.Em.Harmonia.Domain.Validators
+{
+    public class UserValidator : AbstractValidator<UserBaseData>
+    {
+        public UserValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotNull()
+                .Length(2, 255);
+
+            RuleFor(x => x.Email)
+                .EmailAddress(FluentValidation.Validators.EmailValidationMode.AspNetCoreCompatible);
+
+            RuleFor(x => x.Phone)
+                .Length(8, 13);
+
+            RuleFor(x => x.Phone)
+                .NotNull()
+                .Length(8, 13);
+        }
+    }
+}
