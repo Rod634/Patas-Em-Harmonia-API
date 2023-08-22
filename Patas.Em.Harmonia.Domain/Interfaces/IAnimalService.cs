318b3bb05757fdad35ca422e7de928896f0b5d12
rodrigo <rodrigocsilva63@gmail.com>
2023-08-22 19:15:48 -0300

    Create service interface for animals

---
+++ Patas.Em.Harmonia.Domain/Interfaces/IAnimalService.cs
@@ -0,0 +1,31 @@
+﻿using Patas.Em.Harmonia.Domain.Models;
+
+namespace Patas.Em.Harmonia.Domain.Interfaces
+{
+    public interface IAnimalService
+    {
+        /// <summary>
+        /// Get animal by user
+        /// </summary>
+        /// <returns>Returns an User Animals</returns>
+        Task<List<Animal>> GetAnimalsFromAUser(string email);
+
+        /// <summary>
+        /// Get all animals
+        /// </summary>
+        /// <returns>Returns all animals</returns>
+        Task<List<Animal>> GetAllAnimals();
+
+        /// <summary>
+        /// Create a animal
+        /// </summary>
+        /// <returns>A bool that indicates success or not</returns>
+        Task<bool> CreateAnimal(Animal animal);
+
+        /// <summary>
+        /// ChangeAnimalStatus
+        /// </summary>
+        /// <returns>A bool that indicates success or not</returns>
+        Task<bool> ChangeAnimalState(string status);
+    }
+}
