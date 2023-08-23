f7de6c6199d8b9dba9522054552353f530610972
rodrigo <rodrigocsilva63@gmail.com>
2023-08-23 20:51:31 -0300

    Change animal Age to int

--- Patas.Em.Harmonia.Domain/Models/Entities/Animal.cs
+++ Patas.Em.Harmonia.Domain/Models/Entities/Animal.cs
@@ -31,7 +31,7 @@ namespace Patas.Em.Harmonia.Domain.Models.Entities
 
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Age { get; set; }
+        public int Age { get; set; }
         public string Race { get; set; }
         public string Species { get; set; }
         public string Gender { get; set; }
