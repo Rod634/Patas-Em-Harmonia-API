4eeab90ea3b0b489c75c799b0995920b640df5fa
rodrigo <rodrigocsilva63@gmail.com>
2023-08-23 21:43:50 -0300

    Ngo service funtions

--- Patas.Em.Harmonia.Domain/Models/Entities/Ngo.cs
+++ Patas.Em.Harmonia.Domain/Models/Entities/Ngo.cs
@@ -1,11 +1,24 @@
 ﻿#nullable disable
 
-using Patas;
+using Patas.Em.Harmonia.Domain.Constants;
 
 namespace Patas.Em.Harmonia.Domain.Models.Entities
 {
     public partial class Ngo
     {
+        public Ngo(NgoData ngo)
+        {
+            Name = ngo.Name;
+            Services = ngo.Services;
+            Phone = ngo.Phone;
+            Email = ngo.Email;
+            Address = ngo.Address;
+            LatitudeLongitude = ngo.LatitudeLongitude;
+            Neighborhood = ngo.Neighborhood;
+            Status = Constant.PENDANT;
+            Created = DateTime.UtcNow;
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public string Services { get; set; }
