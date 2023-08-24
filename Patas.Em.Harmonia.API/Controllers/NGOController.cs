7eb374d01b7fa10311da3e6e6af697e98bcb01fa
rodrigo <rodrigocsilva63@gmail.com>
2023-08-23 21:46:27 -0300

    Ngo Controller

---
+++ Patas.Em.Harmonia.API/Controllers/NGOController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Patas.Em.Harmonia.Domain.Interfaces;
+using Patas.Em.Harmonia.Domain.Models;
+using Patas.Em.Harmonia.Domain.Models.Entities;
+
+namespace Patas.Em.Harmonia.API.Controllers
+{
+    [ApiController]
+    [Route("v1/[controller]")]
+    public class NgoController : ControllerBase
+    {
+        private readonly INGOService _nGOService;
+
+        public NgoController(INGOService nGOService)
+        {
+            _nGOService = nGOService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateNgoAsync([FromBody] NgoData ngo)
+        {
+            var response = await _nGOService.CreateNgo(ngo);
+            return response ? StatusCode(201) : StatusCode(400);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllNgosAsync()
+        {
+            var response = await _nGOService.GetAllNgos();
+            return Ok(response);
+        }
+    }
+}
