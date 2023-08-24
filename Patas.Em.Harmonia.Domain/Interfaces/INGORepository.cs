206d3f628ab2734ab16a44d606c1d5a50f17bce9
rodrigo <rodrigocsilva63@gmail.com>
2023-08-23 21:25:54 -0300

    NGO repository interface

---
+++ Patas.Em.Harmonia.Domain/Interfaces/INGORepository.cs
@@ -0,0 +1,19 @@
+﻿using Patas.Em.Harmonia.Domain.Models.Entities;
+
+namespace Patas.Em.Harmonia.Domain.Interfaces
+{
+    public interface INGORepository
+    {
+        /// <summary>
+        /// Create a NGO
+        /// </summary>
+        /// <returns>A bool that indicates success or not</returns>
+        Task<bool> CreateNGO(Ngo ngo);
+
+        /// <summary>
+        /// Get all NGO's
+        /// </summary>
+        /// <returns>A List with NGO's</returns>
+        Task<List<Ngo>> GetAllNGOs();
+    }
+}
