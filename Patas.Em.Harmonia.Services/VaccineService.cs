3a3deac26fda9ed449063f81175e1d176f85175e
rodrigo <rodrigocsilva63@gmail.com>
2023-08-25 21:40:35 -0300

    Create vaccineServices

---
+++ Patas.Em.Harmonia.Services/VaccineService.cs
@@ -0,0 +1,19 @@
+﻿using Patas.Em.Harmonia.Domain.Interfaces;
+
+namespace Patas.Em.Harmonia.Services
+{
+    public class VaccineService : IVaccineService
+    {
+        private readonly IVaccineRepository _vaccineRepository;
+
+        public VaccineService(IVaccineRepository vaccineRepository)
+        {
+            _vaccineRepository = vaccineRepository;
+        }
+
+        public Task<List<string>> GetAllVaccines()
+        {
+            return _vaccineRepository.GetAllVaccines();
+        }
+    }
+}
