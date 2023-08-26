6a1ae393cace25df98ca5d863784f4ac3ec480cf
rodrigo <rodrigocsilva63@gmail.com>
2023-08-25 22:04:33 -0300

    Service and repo

---
+++ Patas.Em.Harmonia.Services/DiseaseService.cs
@@ -0,0 +1,19 @@
+﻿using Patas.Em.Harmonia.Domain.Interfaces;
+
+namespace Patas.Em.Harmonia.Services
+{
+    public class DiseaseService : IDiseaseService
+    {
+        private readonly IDiseaseRepository _diseaseRepository;
+
+        public DiseaseService(IDiseaseRepository diseaseRepository)
+        {
+            _diseaseRepository = diseaseRepository;
+        }
+
+        public Task<List<string>> GetAllDiseaseNames()
+        {
+            return _diseaseRepository.GetAllDiseaseNames();
+        }
+    }
+}
