7adcc63f508cc0eda4f2e16ca705f05594c7d17d
rodrigo <rodrigocsilva63@gmail.com>
2023-08-25 22:01:58 -0300

    Disease interfaces

---
+++ Patas.Em.Harmonia.Domain/Interfaces/IDiseaseService.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Patas.Em.Harmonia.Domain.Interfaces
+{
+    public interface IDiseaseService
+    {
+        /// <summary>
+        /// Get all Disease names
+        /// </summary>
+        /// <returns>Returns a list with all Disease names</returns>
+        Task<List<string>> GetAllDiseaseNames();
+    }
+}
