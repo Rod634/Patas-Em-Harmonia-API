29dc4c479240663827d275d119e7ed8cf8023488
rodrigo <rodrigocsilva63@gmail.com>
2023-08-25 21:55:37 -0300

    Create status service

---
+++ Patas.Em.Harmonia.Domain/Interfaces/IStatusService.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Patas.Em.Harmonia.Domain.Interfaces
+{
+    public interface IStatusService
+    {
+        /// <summary>
+        /// Get all status names
+        /// </summary>
+        /// <returns>Returns a list with all status names</returns>
+        Task<List<string>> GetAllStatus();
+    }
+}
---
+++ Patas.Em.Harmonia.Services/StatusService.cs
@@ -0,0 +1,19 @@
+﻿using Patas.Em.Harmonia.Domain.Interfaces;
+
+namespace Patas.Em.Harmonia.Services
+{
+    public class StatusService : IStatusService
+    {
+        private readonly IStatusRepository _statusRepository;
+
+        public StatusService(IStatusRepository statusRepository)
+        {
+            _statusRepository = statusRepository;
+        }
+
+        public Task<List<string>> GetAllStatus()
+        {
+            return _statusRepository.GetAllStatus();
+        }
+    }
+}
